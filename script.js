6610a473e32eabbc5877b06c9d4abbe42da0d309
Vinglish <khan.ildar94@gmail.com>
2018-02-23 12:17:45 +0200

    Practic

---
+++ script.js
@@ -0,0 +1,7 @@
+var l = 0;
+var P = 3.14;
+var r = Number(prompt("Введите радиус круга:"));
+ l = 2*P*r;
+ S = P*r*r;
+ document.write("Длина круга:" + (l).toFixed(1) +"<br />");
+ document.write("Площадь круга:" + (S).toFixed(1));
